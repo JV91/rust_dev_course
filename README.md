06100ea1083acbc78c23a0dc6333ff9bc010c8e6
Jan Vais <jan.vais@ixtent.com>
2023-11-06 23:14:18 +0100

    Intitial commit

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Exercise 1: [Simple "Hello, user" project with colored terminal output](lesson-0
 
 Exercise 2: [CLI program with input text transformation](lesson-02/src/main.rs) 
 
-Exercise 3: [Enhanced CLI program with error handling](lesson-03/src/main.rs) 
+Exercise 3: [Enhanced CLI program with error handling](lesson-05/src/main.rs) 
+
+Exercise 4: [Advancing our program with multi-threading and CVS file reading](lesson-07/src/main.rs)
+
+Exercise 5: [Network I/O](lesson-09/src/main.rs)
---
+++ lesson-09/src/main.rs
@@ -0,0 +1,101 @@
+use std::error::Error;
+use std::fmt;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+//use std::thread;
+//use std::time::Duration;
+
+// Custom Error type for the operations
+#[derive(Debug)]
+struct OperationError(String);
+
+impl fmt::Display for OperationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Operation Error: {}", self.0)
+    }
+}
+
+impl Error for OperationError {}
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: &str) -> Self {
+        Server {
+            address: address.to_string(),
+        }
+    }
+
+    fn start(&self) -> Result<(), Box<dyn Error>> {
+        let listener = TcpListener::bind(&self.address)?;
+        println!("Server listening on {}", self.address);
+
+        for stream in listener.incoming() {
+            let stream = stream?;
+            self.handle_client(stream)?;
+        }
+
+        Ok(())
+    }
+
+    fn handle_client(&self, _stream: TcpStream) -> Result<(), Box<dyn Error>> {
+        // Handle client messages and file transfers here
+        // TODO: Implement messaging protocol logic
+        Ok(())
+    }
+}
+
+struct Client {
+    address: String,
+}
+
+impl Client {
+    fn new(address: &str) -> Self {
+        Client {
+            address: address.to_string(),
+        }
+    }
+
+    fn send_message(&self, message: &str) -> Result<(), Box<dyn Error>> {
+        let mut stream = TcpStream::connect(&self.address)?;
+
+        // Send the message to the server
+        stream.write_all(message.as_bytes())?;
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: {} <server/client> <address>", args[0]);
+        std::process::exit(1);
+    }
+
+    let mode = &args[1];
+    let address = &args[2];
+
+    match mode.as_str() {
+        "server" => {
+            let server = Server::new(address);
+            if let Err(err) = server.start() {
+                eprintln!("Server error: {}", err);
+            }
+        }
+        "client" => {
+            let client = Client::new(address);
+            // Example client message sending
+            if let Err(err) = client.send_message("Hello, Server!") {
+                eprintln!("Client error: {}", err);
+            }
+        }
+        _ => {
+            println!("Invalid mode. Use 'server' or 'client'.");
+            std::process::exit(1);
+        }
+    }
+}
