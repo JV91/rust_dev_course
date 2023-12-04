608ad4fe73f27cc957b7f63f2adb351d9f3cfff0
Jan Vais <jan.vais@ixtent.com>
2023-12-04 15:24:25 +0100

    Remove target folders from version control

--- lesson-09/server/target/debug/deps/chatapp_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jan.vais\Desktop\codebase\!rust_dev_course\lesson-09\chatapp_server\target\debug\deps\chatapp_server.exe: src/server.rs
-
-C:\Users\jan.vais\Desktop\codebase\!rust_dev_course\lesson-09\chatapp_server\target\debug\deps\chatapp_server.d: src/server.rs
-
-src/server.rs:
